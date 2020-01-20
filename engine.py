a009930c99dd51f01c361cd8510515ceb2e53f35
F-G Fernandez <fgfm03@hotmail.fr>
2020-01-20 22:43:54 +0100

    feat: Add lane detection script
    Add python functions and main script for lane detection on both image and videos.

---
+++ engine.py
@@ -0,0 +1,275 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+'''
+Lane detection functions
+'''
+
+import math
+import cv2
+import numpy as np
+import matplotlib.image as mpimg
+from functools import partial
+from moviepy.editor import VideoFileClip
+
+
+def convert_colorspace(img, colorspace='grayscale'):
+    """Converts RGB image to another colorspace
+
+    Args:
+        img (numpy.ndarray): input RGB image
+        colorspace (str, optional): target colorspace
+
+    Returns:
+        numpy.ndarray: converted image
+    """
+
+    if colorspace == 'grayscale':
+        cspace_code = cv2.COLOR_RGB2GRAY
+    elif colorspace == 'hsv':
+        cspace_code = cv2.COLOR_RGB2HSV
+    elif colorspace == 'hsl':
+        cspace_code = cv2.COLOR_RGB2HSL
+
+    return cv2.cvtColor(img, cspace_code)
+
+
+def overlay_mask(img, vertices):
+    """Applies masking on image. Only keeps the region of the image defined by the polygon
+    formed from `vertices`. The rest of the image is set to black.
+    `vertices` should be a numpy array of integer points.
+
+    Args:
+        img (numpy.ndarray): input image
+        vertices (numpy.ndarray): vertices of the mask to apply
+
+    Returns:
+        numpy.ndarray: resulting image
+    """
+    # defining a blank mask to start with
+    mask = np.zeros_like(img)
+
+    # defining a 3 channel or 1 channel color to fill the mask with depending on the input image
+    if len(img.shape) > 2:
+        channel_count = img.shape[2]  # i.e. 3 or 4 depending on your image
+        ignore_mask_color = (255,) * channel_count
+    else:
+        ignore_mask_color = 255
+
+    # filling pixels inside the polygon defined by "vertices" with the fill color
+    cv2.fillPoly(mask, vertices, ignore_mask_color)
+
+    # returning the image only where mask pixels are nonzero
+    masked_image = cv2.bitwise_and(img, mask)
+    return masked_image
+
+
+def draw_lines(img, lines, color=[0, 255, 0], thickness=3):
+    """Draw lines on the image inplace
+
+    Args:
+        img (numpy.ndarray): input image
+        lines (list<tuple>): lines to draw on image
+        color (list<int>, optional): color of lines in RGB
+        thickness (int, optional): thickness of lines drawn
+    """
+
+    for x1, y1, x2, y2 in lines:
+        cv2.line(img, (x1, y1), (x2, y2), color, thickness)
+
+
+def extrapolate_lines(lines, img_shape, min_slope=0.3):
+    """Aggregate detectes lines into two lanes by extrapolation
+
+    Args:
+        lines (list<list<tuple>>): detected lines
+        img_shape (tuple<int>): image shape
+        min_slope (float, optional): minimum slope to consider the edge as being part of the lane
+
+    Returns:
+        list<tuple>: list of extrapolated lanes
+    """
+
+    extrapolated_lines = []
+
+    # Store slopes
+    left_slopes, right_slopes = [], []
+    left_intercepts, right_intercepts = [], []
+    ymin = img_shape[0]
+    for line in lines:
+        for x1, y1, x2, y2 in line:
+            slope = (y2 - y1) / (x2 - x1)
+            intercept = y2 - slope * x2
+            # Reduce noise for outliers
+            if min_slope < abs(slope) < math.inf:
+                # Check if left lane
+                if slope < 0:
+                    # Double check to avoid noise from other part of the image
+                    if max(x1, x2) < img_shape[1] / 2:
+                        left_slopes.append(slope)
+                        left_intercepts.append(intercept)
+                else:
+                    if min(x1, x2) > img_shape[1] / 2:
+                        right_slopes.append(slope)
+                        right_intercepts.append(intercept)
+                ymin = min(ymin, y1, y2)
+
+    if len(left_slopes) > 0:
+        # Average slope and intercept
+        left_slope = sum(left_slopes) / len(left_slopes)
+        left_intercept = sum(left_intercepts) / len(left_intercepts)
+        # Add the extrapolated lane
+        left = (int((img_shape[0] - left_intercept) / left_slope), img_shape[0],
+                int((ymin - left_intercept) / left_slope), int(ymin))
+        extrapolated_lines.append(left)
+
+    if len(right_slopes) > 0:
+        right_slope = sum(right_slopes) / len(right_slopes)
+        right_intercept = sum(right_intercepts) / len(right_intercepts)
+        right = (int((img_shape[0] - right_intercept) / right_slope), img_shape[0],
+                 int((ymin - right_intercept) / right_slope), int(ymin))
+        extrapolated_lines.append(right)
+
+    return extrapolated_lines
+
+
+def hough_lines(img, rho=2, theta=np.pi / 180, threshold=20, min_line_len=5, max_line_gap=25, thickness=3):
+    """Perform a Hough transform on img
+
+    Args:
+        img (numpy.ndarray): input image
+        rho (float, optional): distance resolution in pixels of the Hough grid
+        theta (float, optional): angular resolution in radians of the Hough grid
+        threshold (float, optional): minimum number of votes (intersections in Hough grid cell)
+        min_line_len (int, optional): minimum number of pixels making up a line
+        max_line_gap (int, optional): maximum gap in pixels between connectable line segments
+        thickness (int, optional): thickness of lines drawn on resulting image
+
+    Returns:
+        numpy.ndarray: result image
+    """
+    # Hough transform
+    lines = cv2.HoughLinesP(img, rho, theta, threshold, np.array([]),
+                            minLineLength=min_line_len, maxLineGap=max_line_gap)
+    line_img = np.zeros((img.shape[0], img.shape[1], 3), dtype=np.uint8)
+    # Line extrapolation
+    extrapolated_lines = extrapolate_lines(lines, line_img.shape)
+    # Image display
+    draw_lines(line_img, extrapolated_lines, thickness=thickness)
+
+    return line_img
+
+
+def weighted_img(img1, img2, α=0.8, β=1., γ=0.):
+    """Interpolate two images into a single one by applying
+    α * img1 + β * img2 + γ
+
+    Args:
+        img1 (numpy.ndarray[H, W, C]): first image
+        img2 (numpy.ndarray[H, W, C]): second image
+        α (float, optional): weight of first image
+        β (float, optional): weight of second image
+        γ (float, optional): offset
+
+    Returns:
+        numpy.ndarray: resulting image
+    """
+    return cv2.addWeighted(img2, α, img1, β, γ)
+
+
+def get_depth_vertices(img_shape, lat_offset=0.08, horizon=(0.55, 0.5), vert_range=(0.62, 0.9)):
+    """Compute depth view vertices
+
+    Args:
+        img_shape (tuple<int>): shape of the input image
+        lat_offset (float, optional): relative lateral offset of the bottom of the mask
+        horizon (tuple<float>, optional): relative coordinates of apparent horizon
+        vert_range (tuple<float>, optional): relative range of vertical masking
+
+    Returns:
+        numpy.ndarray: vertices of depth view mask
+    """
+
+    # Compute cut coordinates
+    leftcut_min = lat_offset + (1 - vert_range[0]) / (1 - horizon[0]) * (horizon[1] - lat_offset)
+    leftcut_max = lat_offset + (1 - vert_range[1]) / (1 - horizon[0]) * (horizon[1] - lat_offset)
+
+    vertices = np.array([[
+        (leftcut_max * img_shape[1], vert_range[1] * img_shape[0]),
+        (leftcut_min * img_shape[1], vert_range[0] * img_shape[0]),
+        ((1 - leftcut_min) * img_shape[1], vert_range[0] * img_shape[0]),
+        ((1 - leftcut_max) * img_shape[1], vert_range[1] * img_shape[0])]], dtype=np.int32)
+
+    return vertices
+
+
+def _process_image(img, colorspace='hsv', thickness=3):
+    """Compute the lane mask of an input image
+
+    Args:
+        img (numpy.ndarray[H, W, C]): input image
+        colorspace (str, optional): colorspace to use for canny edge detection
+        thickness (int, optional): thickness of lines on result image
+
+    Returns:
+        numpy.ndarray[H, W, 3]: lane mask
+    """
+
+    # Grayscale conversion
+    cspace_img = convert_colorspace(img, colorspace)
+    # Gaussian smoothing
+    smooth_img = cv2.GaussianBlur(cspace_img, (3, 3), 0)
+
+    # Colorspace masking
+    if colorspace == 'hsv':
+        yellow_hsv_low = np.array([0, 100, 100])
+        yellow_hsv_high = np.array([50, 255, 255])
+
+        white_hsv_low = np.array([20, 0, 180])
+        white_hsv_high = np.array([255, 80, 255])
+
+        yellow_mask = cv2.inRange(smooth_img, yellow_hsv_low, yellow_hsv_high)
+        white_mask = cv2.inRange(smooth_img, white_hsv_low, white_hsv_high)
+
+        smooth_img = cv2.bitwise_or(yellow_mask, white_mask)
+
+    # Canny edge detection
+    canny_img = cv2.Canny(smooth_img, 50, 150)
+
+    # Apply depth view masking
+    vertices = get_depth_vertices(img.shape)
+    masked_edges = overlay_mask(canny_img, vertices)
+
+    # Run Hough on edge detected image
+    # Output "lines" is an array containing endpoints of detected line segments
+    return hough_lines(masked_edges, thickness=thickness)
+
+
+def process_image(img_path, thickness=3):
+    """Display lane detection results on input image
+
+    Args:
+        img (str): input image path
+        thickness (int, optional): thickness of lines on result image
+
+    Returns:
+        numpy.ndarray[H, W, 3]: input image overlayed with result
+    """
+
+    img = mpimg.imread(img_path)
+
+    return weighted_img(img, _process_image(img, thickness=thickness))
+
+
+def process_video(video_path, output_file, thickness=3):
+    """Display lane detection results on input image
+
+    Args:
+        video_path (str): input video path
+        output_file (str): output video path
+        thickness (int, optional): thickness of lines on result image
+    """
+
+    video = VideoFileClip(video_path)
+    clip = video.fl_image(partial(process_image, thickness=thickness))
+    clip.write_videofile(output_file, audio=False)
---
+++ main.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+'''
+Lane detection script
+'''
+
+import argparse
+import matplotlib.image as mpimg
+from engine import process_image, process_video
+
+
+def main(args):
+
+    outfile = args.dest
+    # Append input extension to default destination fine
+    if len(outfile.split('/')[-1].split('.')) == 1:
+        extension = args.source.split('.')[-1]
+        outfile = f"{args.dest}.{extension}"
+
+    # Allow video processing using the args.video flag
+    if args.video:
+        process_video(args.source, outfile, thickness=args.thickness)
+    else:
+        res = process_image(args.source, thickness=args.thickness)
+        mpimg.imsave(outfile, res)
+
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(description='Lane detector',
+                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument("source", type=str, help="File to process")
+    parser.add_argument("--dest", type=str, default='lane-detection-result', help="Output file name")
+    parser.add_argument("--video", dest="video", help="should the source be processed as a video",
+                        action="store_true")
+    parser.add_argument("--thickness", type=int, default=5, help="Thickness of drawn lines")
+
+    args = parser.parse_args()
+    main(args)
